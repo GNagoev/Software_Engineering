444be56da826458d1b6e167badf3c9b7b159db83
Gleb <gleb_nagoev@mail.ru>
2024-09-25 20:41:35 +0500

    123

--- Lab3_3.py
+++
@@ -1,6 +0,0 @@
-numbers = [1, 2, 3, 4, 5]
-value = int(input("Введите значение переменной: "))
-if value in numbers:
-  print(f"Переменная {value} есть в массиве")
-else:
-  print(f"Переменной {value} нет в массиве")
--- Sam3_1.py
+++
@@ -1,5 +0,0 @@
-a = 1
-for i in range(1):
-  a *= 30
-  a += 1
-print(a)
