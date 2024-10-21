9af72b590647b0c8e139305b9da28a00b813bb09
GNagoev <152078099+GNagoev@users.noreply.github.com>
2024-10-21 22:38:00 +0500

    Update Sam1.py

--- Sam1.py
+++ Sam1.py
@@ -1,9 +1,23 @@
-user_input = input("Введите последовательность чисел, разделенных пробелом: ")
+from collections import Counter
+import re
 
-number_strings = user_input.split()
-numbers = [int(num) for num in number_strings]
 
-numbers_tuple = tuple(numbers)
+def analyze_text_file(directory):
+    try:
+        with open(directory, 'r', encoding='utf-8') as file:
+            text = file.read()
 
-print("Список:", numbers)
-print("Кортеж:", numbers_tuple)
+        words = re.findall(r'\b\w+\b', text.lower())
+
+        word_count = len(words)
+        word_frequency = Counter(words)
+
+        most_common_word, most_common_count = word_frequency.most_common(1)[0]
+
+        print(f"Количество слов в файле: {word_count}")
+        print(f"Самое часто встречающееся слово: '{most_common_word}' (встречается {most_common_count} раз)")
+
+    except FileNotFoundError:
+        print("Файл не найден. Проверьте путь к файлу.")
+
+analyze_text_file(r'C:\Users\Глеб\PycharmProjects\lab_1\input.txt')
