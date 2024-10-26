b13bb43e2f6d3eb35d10892e1b661da04e64c07a
GNagoev <152078099+GNagoev@users.noreply.github.com>
2024-10-26 14:58:19 +0500

    Update Sam2.py

--- Sam2.py
+++ Sam2.py
@@ -1,1 +1,9 @@
+class Human:
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
+    def introduce(self):
+        print((f"Hi, my name is {self.name}, i am {self.age} years old."))
 
+Gleb = Human("23", "Gleb")
+Gleb.introduce()
