bb4ff706c5e7016b48cdd5137697bca772fb0326
GNagoev <152078099+GNagoev@users.noreply.github.com>
2024-11-02 00:13:50 +0500

    Update Lab5.py

--- Lab5.py
+++ Lab5.py
@@ -1,25 +1,20 @@
-class Shape:  # Определяем общий класс Shape
-    def area(self):  # Определяем метод для расчета площади (будет переопределен в дочерних классах)
-        pass
+class Russian:
+    @staticmethod
+    def greeting():
+        print("Привет")
 
-class Rectangle(Shape):  # Определяем класс Rectangle, наследующий от Shape
-    def __init__(self, width, height):  # Метод инициализации, принимающий ширину и высоту
-        self.width = width  # Сохраняем ширину
-        self.height = height  # Сохраняем высоту
 
-    def area(self):  # Переопределяем метод area для расчета площади прямоугольника
-        return self.width * self.height  # Возвращаем площадь
+class English:
+    @staticmethod
+    def greeting():
+        print("Hello")
 
-class Circle(Shape):  # Определяем класс Circle, наследующий от Shape
-    def __init__(self, radius):  # Метод инициализации, принимающий радиус
-        self.radius = radius  # Сохраняем радиус
 
-    def area(self):  # Переопределяем метод area для расчета площади круга
-        return 3.14 * self.radius * self.radius  # Возвращаем площадь
+def greet(language):
+    language.greeting()
 
-# Создаем массив с фигурами
-shapes = [Rectangle(5, 4), Circle(3)]  # Добавляем экземпляры прямоугольника и круга
 
-# Выводим площади фигур
-for shape in shapes:  # Проходим по каждой фигуре в массиве
-    print(f"The area is: {shape.area()}")  # Вызываем метод area и выводим площадь
+ivan = Russian()
+greet(ivan)
+john = English()
+greet(john)
