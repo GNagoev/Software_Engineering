be24c9d914df8e9bf6dc2f943876342d92c4bfe6
GNagoev <152078099+GNagoev@users.noreply.github.com>
2024-11-02 21:27:53 +0500

    Update Sam2.py

--- Sam2.py
+++ Sam2.py
@@ -1,10 +1,14 @@
+class Tomato:
+    states = ['отсутствует', 'цветение', 'зеленый', 'красный']
+
+    def __init__(self, index):
+        self._index = index  # Динамическое свойство: индекс помидора
+        self._state = self.states[0]  # Динамическое свойство: начальная стадия созревания (отсутствует)
+
     def grow(self):
-        """Переводит томат на следующую стадию созревания."""
         current_index = self.states.index(self._state)
         if current_index < len(self.states) - 1:
             self._state = self.states[current_index + 1]
 
     def is_ripe(self):
-        """Проверяет, что томат созрел."""
         return self._state == 'красный'
-
