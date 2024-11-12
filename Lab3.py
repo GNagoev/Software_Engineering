4ee01b2e4694aba73ec9d26888dd15eda0f2e072
GNagoev <152078099+GNagoev@users.noreply.github.com>
2024-11-13 01:32:39 +0500

    Update Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -1,31 +1,15 @@
-class MyClass:
-    def __init__(self, value):
-        self._value = value
+def data(*args):
+    try:
+        for i in range(len(*args)):
+            try:
+                result = (args[0][i] *15)//10
+                print(result)
+            except Exception as ex:
+                print(ex)
+    except Exception as ex:
+        print(ex)
+    finally:
+        print('Вся информация обработана')
 
-    def set_value(self, value):
-        self._value = value
-
-    def get_value(self):
-        if hasattr(self, '_value'):
-            return self._value
-        else:
-            raise AttributeError("Значение было удалено")
-
-    def del_value(self):
-        del self._value
-
-    value = property(get_value, set_value, del_value, "Свойство value")
-
-
-obj = MyClass(42)
-print(obj.value)
-obj.set_value(45)
-print(obj.value)
-obj.set_value(100)
-print(obj.value)
-obj.del_value()
-try:
-    print(obj.value)
-except AttributeError as e:
-    print(e)
-#Проблема была в том что геттер принимал value, и в том что после удаления value, мы пытались вывести его без проверки его удалдения.
+if __name__ == '__main__':
+    data([1,15, 'Hello', 'i', 'try', 'to', 'crash', 'your','site', 38, 45])
