b96cbfa0a98aac4f1193dbcd189a7980cefd23d1
GNagoev <152078099+GNagoev@users.noreply.github.com>
2024-11-13 01:49:45 +0500

    Update Sam1.py

--- Sam1.py
+++ Sam1.py
@@ -1,6 +1,20 @@
-class Tomato:
-    states = ['отсутствует', 'цветение', 'зеленый', 'красный']
-    
-    def __init__(self, index):
-        self._index = index  # Динамическое свойство: индекс помидора
-        self._state = self.states[0]  # Динамическое свойство: начальная стадия созревания (отсутствует)
+import time
+
+def calculate_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f" Function {func.__name__} took {end_time - start_time} seconds to execute")
+        return result
+    return wrapper
+
+@calculate_time
+def fibonacci():
+    fib1 = fib2 = 1
+    for i in range(2, 200):
+        fib1, fib2 = fib2, fib1 + fib2
+        print(fib2, end='')
+
+if __name__ == '__main__':
+    fibonacci()
